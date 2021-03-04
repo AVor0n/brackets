37472d3b2b9fe8fba43747ce12b894016273deeb
AVor0n <AVor0n@yandex.ru>
2021-03-05 02:16:53 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketSamples = bracketsConfig.map((e) => e.join(''));
+
+  for (let i = 0; i < bracketSamples.length; i++){
+    if (str.includes(bracketSamples[i])) {
+      str = str.replace(bracketSamples[i], '');
+      i = -1;
+    }
+  }
+
+  return str ? false : true;
 }
